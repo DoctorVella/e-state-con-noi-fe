f3fdcd9c333b242fcf8babe0baf5c5f99a3159f6
DoctorVella <federicov96@gmail.com>
2022-12-19 14:53:52 +0100

    Style management

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
-import logo from './logo.svg';
 import './App.css';
+import { Box, Button, Card, CardActions, CardContent, Grid, ThemeProvider, Typography } from '@mui/material';
+import { muiTheme } from './util/MuiTheme';
 
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ThemeProvider theme={muiTheme}>
+      <div className="App">
+        <Box sx={{ p: 3 }}>
+          <Box sx={{ pb: 3 }}>
+            <Typography variant='title'>E...STATE CON NOI</Typography>
+            <Typography variant='h2'>parrocchia Santa Fara</Typography>
+          </Box>
+          <Grid container rowSpacing={2} columnSpacing={5}>
+            <Grid item xs={0} md={2} />
+            <Grid item xs={12} md={8}>
+              <Typography variant='paragraph' paragraph>
+                Benvenuto sul sito di E...State Con Noi, il campo estivo per ragazzi organizzato dalla parrocchia Santa Fara. Il campo estivo, dedicato ai ragazzi fra i 7 e i 14 anni, si terrà nei seguenti giorni:<br/> 
+                - da lunedì 26 giugno a venerdì 30 giugno<br/> 
+                - da lunedì 2 luglio a venerdì 7 luglio<br/>
+                Dalle ore 16.00 alle ore 20.00.
+              </Typography>
+            </Grid>
+            <Grid item xs={0} md={2} />
+            <Grid item xs={0} md={2} />
+            <Grid item xs={12} md={4}>
+              <Card raised>
+                <CardContent>
+                  <Typography variant='h3'>Non sei ancora registrato?</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button>Registrati</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={12} md={4}>
+              <Card raised>
+                <CardContent>
+                  <Typography variant='h3'>Vuoi accedere alla tua area personale?</Typography>
+                </CardContent>
+                <CardActions>
+                  <Button>Accedi</Button>
+                </CardActions>
+              </Card>
+            </Grid>
+            <Grid item xs={0} md={2} />
+          </Grid>
+        </Box>
+      </div>
+    </ThemeProvider>
   );
 }
 
---
+++ src/util/MuiTheme.js
@@ -0,0 +1,128 @@
+import { createTheme } from "@mui/material";
+
+const orange = '#F7931E';
+const darkOrange = '#F05A28';
+const lightOrange = '#FFF0BC';
+const lightYellow = '#FEFCDB';
+
+const redTeam = '#FC666B';
+const yellowTeam = '#F7E969';
+const greenTeam = '#A2DE5E';
+const blueTeam = '#346CDB';
+
+export const muiTheme = createTheme({
+    typography: {
+        fontFamily: [
+            'Raleway',
+            '-apple-system',
+            'BlinkMacSystemFont',
+            '"Segoe UI"',
+            'Roboto',
+            '"Helvetica Neue"',
+            'Arial',
+            'sans-serif',
+            '"Apple Color Emoji"',
+            '"Segoe UI Emoji"',
+            '"Segoe UI Symbol"'
+        ].join(',')
+    },
+    palette:{
+        primary: {
+            main: orange
+        },
+        darker: {
+            main: darkOrange
+        },
+        lighter: {
+            main: lightOrange
+        },
+        lightest: {
+            main: lightYellow
+        }
+    },
+    components: {
+        MuiButton: {
+            defaultProps: {
+                size: 'medium',
+                variant: 'contained',
+            },
+            variants: [
+                {
+                    props: {variant: 'contained'},
+                    style: {
+                        color: 'white'
+                    }
+                }
+            ]
+        },
+        MuiCardActions: {
+            defaultProps: {
+                style: {
+                    justifyContent: 'center'
+                }
+            }
+        },
+        MuiTypography: {
+            defaultProps: {
+                fontWeight: '700',
+                color: orange
+            },
+            variants: [
+                {
+                    props: {variant: 'title'},
+                    style: {
+                        background: "-webkit-linear-gradient(left, " + redTeam + " 15%, " + yellowTeam + " 40%, " + greenTeam + ", " + blueTeam + " 85%)",
+                        WebkitBackgroundClip: "text",
+                        WebkitTextFillColor: "transparent"
+                    }
+                },
+                {
+                    props: {variant: 'h1'},
+                    style: {
+                        fontSize: '48px'
+                    }
+                },
+                {
+                    props: {variant: 'h2'}
+                },
+                {
+                    props: {variant: 'h3'}
+                },
+                {
+                    props: {variant: 'paragraph'},
+                    style: {
+                        textAlign: 'left'
+                    }
+                }
+            ]
+        }
+    }
+});
+
+muiTheme.typography.title = {
+    fontSize: '32px',
+    [muiTheme.breakpoints.up('md')]: {
+      fontSize: '48px',
+    },
+};
+
+muiTheme.typography.h2 = {
+    fontSize: '24px',
+    [muiTheme.breakpoints.up('md')]: {
+      fontSize: '32px',
+    },
+};
+
+muiTheme.typography.h3 = {
+    fontSize: '24px',
+    [muiTheme.breakpoints.up('md')]: {
+      fontSize: '24px',
+    },
+};
+
+muiTheme.typography.paragraph = {
+    fontSize: '20px',
+    [muiTheme.breakpoints.up('md')]: {
+      fontSize: '24px',
+    },
+}
