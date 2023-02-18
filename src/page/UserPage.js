2d6f1c9063b9334f00e0d24924b1be66ce7539ce
DoctorVella <federicov96@gmail.com>
2023-02-18 12:06:11 +0100

    AdminPage different from AnimatorePage

--- src/page/UserPage.js
+++ src/page/UserPage.js
@@ -1,11 +1,23 @@
-import { AUTH_LEVEL_ADMIN } from "../util/Constants";
+import { AUTH_LEVEL_ADMIN, AUTH_LEVEL_ANIMATORE } from "../util/Constants";
 import AdminPage from "./AdminPage";
+import AnimatorePage from "./AnimatorePage";
 import IscrittorePage from "./IscrittorePage";
 
 const UserPage = () => {
 
+    const getPage = () => {
+        let authLevel = localStorage.getItem("authLevel");
+        if(authLevel === AUTH_LEVEL_ADMIN) {
+            return <AdminPage/>;
+        }else if(authLevel === AUTH_LEVEL_ANIMATORE) {
+            return <AnimatorePage/>;
+        }else{
+            return <IscrittorePage/>;
+        }
+    }
+
     return (<>
-        {localStorage.getItem("authLevel") === AUTH_LEVEL_ADMIN ? <AdminPage/> : <IscrittorePage/>}
+        {getPage()}
     </>);
 }
 
